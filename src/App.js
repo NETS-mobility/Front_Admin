629d7b2ad8ca91aea831316111b32c931b912595
ssu00 <rachel4535@naver.com>
2022-02-11 13:23:07 +0900

    🚚 change folder name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import Editor from "./components/editor";
-import TableList from "./components/tableList";
-import Layout from "./layout/layout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/login/login";
-import ReservationList from "./pages/reservation/reservationList";
+
+// import Login from "./pages/login/login";
+// import ReservationList from "./pages/reservation/reservationList";
+
 import {
   MemberList,
   ManagerList,
   CarList,
   AdminList,
-} from "./screens/management";
+} from "./pages/management";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact={true} element={<Layout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reservationList" element={<ReservationList />} />
+        {/* <Route path="/" exact={true} element={<Layout />} /> */}
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/reservationList" element={<ReservationList />} /> */}
         <Route path="/" element={<ManagerList />} />
         <Route path="/manage/member/list" element={<MemberList />} />
         <Route path="/manage/manager/list" element={<ManagerList />} />
