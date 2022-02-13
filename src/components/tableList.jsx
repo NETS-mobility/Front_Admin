e6a7fef297112bb678999de0e5746ecd0a6435a7
ssu00 <rachel4535@naver.com>
2022-02-13 22:35:11 +0900

    ⚡️ add & edit management's common components

--- src/components/tableList.jsx
+++ src/components/tableList.jsx
@@ -3,68 +3,27 @@ import { Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./tableList.css";
 
-/*
 const TableList = ({
-    ischeck : bool (true이면 checkbox사용, false이면 checkbox X)
-    title1~title4 : string
-})
-*/
-
-const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
+  ischeck,
+  title1,
+  title2,
+  title3,
+  title4,
+  info,
+  baseURL,
+}) => {
   const [checkedList, setCheckedLists] = useState([]);
-  /*
-    const information에 있는 정보가 출력됨
-    column3개로 사용하려면 title4 입력X
-
-    const information = [
-        id: 순서,
-        nav: "이동할 페이지 주소",
-        item1: "title1의 내용"
-        item2: "title2의 내용",
-        item3: "title3의 내용",
-        item4: "title4의 내용"
-    ]
-    */
-
-  const information = [
-    {
-      id: 1,
-      nav: "./login",
-      item1: "zoowb",
-      item2: "최지우",
-      item3: "2022-02-08",
-    },
-    { id: 2, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 3, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 4, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 5, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 6, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 7, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 8, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 9, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 10, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 11, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 12, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 13, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 14, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 15, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 16, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 17, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 18, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-    { id: 19, nav: "./", item1: "zoowb", item2: "최지우", item3: "2022-02-08" },
-  ]; //example
-
   const onCheckedAll = useCallback(
     (checked) => {
       if (checked) {
         const checkedListArray = [];
-        information.forEach((list) => checkedListArray.push(list.id));
+        info.forEach((list) => checkedListArray.push(list.id));
         setCheckedLists(checkedListArray);
       } else {
         setCheckedLists([]);
       }
     },
-    [information]
+    [info]
   );
 
   const onCheckedElement = useCallback(
@@ -78,7 +37,7 @@ const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
     [checkedList]
   );
 
-  const infoList = information.map((info, index) =>
+  const infoList = info.map((info, index) =>
     title4 ? (
       <tr key={index}>
         {ischeck ? (
@@ -96,26 +55,47 @@ const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
         ) : (
           <></>
         )}
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item1}</button>
-          </Link>
-        </td>
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item2}</button>
-          </Link>
-        </td>
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item3}</button>
-          </Link>
-        </td>
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item4}</button>
-          </Link>
-        </td>
+
+        {baseURL ? (
+          <>
+            {" "}
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item1}</button>
+              </Link>
+            </td>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item2}</button>
+              </Link>
+            </td>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item3}</button>
+              </Link>
+            </td>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item4}</button>
+              </Link>
+            </td>
+          </>
+        ) : (
+          <>
+            <td>
+              <button className="tablebutton">{info.item1}</button>
+            </td>
+            <td>
+              <button className="tablebutton">{info.item2}</button>
+            </td>
+            <td>
+              <button className="tablebutton">{info.item3}</button>
+            </td>
+            <td>
+              <button className="tablebutton">{info.item4}</button>
+            </td>
+          </>
+        )}
       </tr>
     ) : (
       <tr key={index}>
@@ -134,21 +114,37 @@ const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
         ) : (
           <></>
         )}
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item1}</button>
-          </Link>
-        </td>
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item2}</button>
-          </Link>
-        </td>
-        <td>
-          <Link to={info.nav}>
-            <button className="tablebutton">{info.item3}</button>
-          </Link>
-        </td>
+        {baseURL ? (
+          <>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item1}</button>
+              </Link>
+            </td>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item2}</button>
+              </Link>
+            </td>
+            <td>
+              <Link to={baseURL + info.nav}>
+                <button className="tablebutton">{info.item3}</button>
+              </Link>
+            </td>
+          </>
+        ) : (
+          <>
+            <td>
+              <button className="tablebutton">{info.item1}</button>
+            </td>
+            <td>
+              <button className="tablebutton">{info.item2}</button>
+            </td>
+            <td>
+              <button className="tablebutton">{info.item3}</button>
+            </td>
+          </>
+        )}
       </tr>
     )
   );
@@ -168,7 +164,7 @@ const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
                       checked={
                         checkedList.length === 0
                           ? false
-                          : checkedList.length === information.length
+                          : checkedList.length === info.length
                           ? true
                           : false
                       }
@@ -203,7 +199,7 @@ const TableList = ({ ischeck, title1, title2, title3, title4 }) => {
                       checked={
                         checkedList.length === 0
                           ? false
-                          : checkedList.length === information.length
+                          : checkedList.length === info.length
                           ? true
                           : false
                       }
