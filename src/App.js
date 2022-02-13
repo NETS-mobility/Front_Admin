a7fab74ce452b6f4de0815b45457720abc71d60a
ssu00 <rachel4535@naver.com>
2022-02-13 22:35:22 +0900

    ⚡️ add router

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import {
   CarList,
   AdminList,
 } from "./pages/management";
+import ManagerDetail from "./pages/management/manager/managerDetail";
+import MemberDetail from "./pages/management/member/memberDetail";
+import ManagerDetailEdit from "./pages/management/manager/managerDetailEdit";
+import AdminDetail from "./pages/management/admin/adminDetail";
+import AdminDetailEdit from "./pages/management/admin/adminDetailEdit";
+import CarDetail from "./pages/management/car/carDetail";
 
 const App = () => {
   return (
@@ -18,10 +24,24 @@ const App = () => {
         {/* <Route path="/login" element={<Login />} />
         <Route path="/reservationList" element={<ReservationList />} /> */}
         <Route path="/" element={<ManagerList />} />
+
         <Route path="/manage/member/list" element={<MemberList />} />
+        <Route path="/member/detail/:id" element={<MemberDetail />} />
+
         <Route path="/manage/manager/list" element={<ManagerList />} />
+        <Route path="/manager/detail/:id" element={<ManagerDetail />} />
+        <Route
+          path="/manager/detail/edit/:id"
+          element={<ManagerDetailEdit />}
+        />
+
         <Route path="/manage/admin/list" element={<AdminList />} />
+        <Route path="/admin/detail/:id" element={<AdminDetail />} />
+        <Route path="/admin/detail/edit/:id" element={<AdminDetailEdit />} />
+
         <Route path="/manage/car/list" element={<CarList />} />
+        <Route path="/car/detail/:id" element={<CarDetail />} />
+        {/* <Route path="/admin/detail/edit/:id" element={<AdminDetailEdit />} /> */}
       </Routes>
     </Router>
   );
