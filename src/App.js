fb8767ec62002294778ad9e694ead6ff98502fa1
ssu00 <rachel4535@naver.com>
2022-03-12 21:58:46 +0900

    ✅  add routers to app.js & add dependencies

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ import ManagerDetailEdit from "./pages/management/manager/managerDetailEdit";
 import AdminDetail from "./pages/management/admin/adminDetail";
 import AdminDetailEdit from "./pages/management/admin/adminDetailEdit";
 import CarDetail from "./pages/management/car/carDetail";
+import CurrentPay from "./pages/pay/currentPay";
+import EditPay from "./pages/pay/editPay";
+import ManagerRegister from "./pages/management/manager/managerRegister";
+import ManagerRegisterComplete from "./pages/management/manager/managerRegisterComplete";
+import CarRegister from "./pages/management/car/carRegister";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000";
 
 const App = () => {
   return (
@@ -28,6 +35,11 @@ const App = () => {
         <Route path="/manage/member/list" element={<MemberList />} />
         <Route path="/member/detail/:id" element={<MemberDetail />} />
 
+        <Route path="/manage/manager/register" element={<ManagerRegister />} />
+        <Route
+          path="/manage/manager/registerComplete"
+          element={<ManagerRegisterComplete />}
+        />
         <Route path="/manage/manager/list" element={<ManagerList />} />
         <Route path="/manager/detail/:id" element={<ManagerDetail />} />
         <Route
@@ -40,8 +52,11 @@ const App = () => {
         <Route path="/admin/detail/edit/:id" element={<AdminDetailEdit />} />
 
         <Route path="/manage/car/list" element={<CarList />} />
+        <Route path="/manage/car/register" element={<CarRegister />} />
         <Route path="/car/detail/:id" element={<CarDetail />} />
-        {/* <Route path="/admin/detail/edit/:id" element={<AdminDetailEdit />} /> */}
+
+        <Route path="/setFee" element={<CurrentPay />} />
+        <Route path="/editFee" element={<EditPay />} />
       </Routes>
     </Router>
   );
